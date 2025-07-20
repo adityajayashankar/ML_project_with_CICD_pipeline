39ae54b3bdd21519f6ed60d58efe1f58162fd48e
Aditya Jayashanakar <adityajayashankar@gmail.com>
2025-07-20 17:48:33 +0530

    HyperParameter Tuning

--- src/components/model_trainer.py
+++ src/components/model_trainer.py
@@ -50,11 +50,20 @@ class ModelTrainer:
                 "AdaBoost Regressor": AdaBoostRegressor(),
                 "K-Neighbors Regressor": KNeighborsRegressor()
             }
-
+            params = {
+                "Linear Regression": {},
+                "Decision Tree Regressor": {"max_depth": [3, 5, 7, 10]},
+                "Random Forest Regressor": {"n_estimators": [50, 100, 200]},
+                "Gradient Boosting Regressor": {"learning_rate": [0.01, 0.1, 0.2]},
+                "XGBoost Regressor": {"n_estimators": [50, 100], "learning_rate": [0.01, 0.1]},
+                "CatBoost Regressor": {"depth": [3, 5, 7], "learning_rate": [0.01, 0.1]},
+                "AdaBoost Regressor": {"n_estimators": [50, 100]},
+                "K-Neighbors Regressor": {"n_neighbors": [3, 5, 7]}
+            }
             model_report: dict = evaluate_models(x_train=x_train,y_train=y_train,x_test=x_test,y_test=y_test,
-                                                models=models)
-            best_model_score = max(model_report.values())
-            best_model_name = list(model_report.keys())[list(model_report.values()).index(best_model_score)]
+                                                models=models,params=params)
+            best_model_score = max([value[0] for value in model_report.values()])
+            best_model_name = [key for key, value in model_report.items() if value[0] == best_model_score][0]
 
             best_model = models[best_model_name]
 
@@ -71,6 +80,6 @@ class ModelTrainer:
             r2_square = r2_score(y_test, predicted)
             return r2_square
             
-        except:
+        except Exception as e:
             raise CustomException(e, sys)
           
